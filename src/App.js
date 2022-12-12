e2cb95afbde5c8725dc6cc47b957d4a2fb68ed30
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2022-12-12 08:58:45 +0530

    little changes have done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>Work in progress !</h1>
-    <h1>Project Code :zealous-fruit-8632</h1>
-    <h1>Stay Connected</h1>
+      <h1>Work in progress !</h1>
+      <h1>Project Code :zealous-fruit-8632</h1>
+      <h1>Stay Connected</h1>
 
-    <h1>
-    # zealous-fruit-8632  `Project Code`
-
-## About Project 
-<h2>
-Sephora is a French multinational retailer of personal care and beauty products. Featuring nearly 340 brands, along with its own private label, Sephora Collection, Sephora offers beauty products including cosmetics, skincare, body, fragrance, nail color, beauty tools, body lotions and haircare
-</h2>
-
-
-# Collaborators
-
-
-# 🧔🏻 **Vikash Kumar Lal** `Team Lead`
-
-- Github:[@Vikash](https://github.com/Therobo77)
-- Linkedin:[Vikash Lal](https://www.linkedin.com/in/vikashlal7722/)
-- Email: (anshusinha8873@gmail.com)
-
-# 👨🏻 **Aman Singh Rajawat** 
-
-- Github:[@Vikash](https://github.com/Therobo77)
-- Linkedin:[Vikash Lal](https://www.linkedin.com/in/vikashlal7722/)
-- Email: (anshusinha8873@gmail.com)
-
-# 🧑🏻‍🦰 **Shobha Malviya**
-
-- Github:[@Vikash](https://github.com/Therobo77)
-- Linkedin:[Vikash Lal](https://www.linkedin.com/in/vikashlal7722/)
-- Email: (anshusinha8873@gmail.com)
-
-# 👱🏻‍♂️ **Rajnandani Kumari**
-
-- Github:[@Vikash](https://github.com/Therobo77)
-- Linkedin:[Vikash Lal](https://www.linkedin.com/in/vikashlal7722/)
-- Email: (anshusinha8873@gmail.com)
-
-# 🧑🏻 **Nitesh Sindhu**
-
-- Github:[@Vikash](https://github.com/Therobo77)
-- Linkedin:[Vikash Lal](https://www.linkedin.com/in/vikashlal7722/)
-- Email: (anshusinha8873@gmail.com)
-
-    </h1>
+      <h1>
+        # zealous-fruit-8632 `Project Code` ## About Project
+        <br/>
+        <span>
+          Sephora is a French multinational retailer of personal care and beauty
+          products. Featuring nearly 340 brands, along with its own private
+          label, Sephora Collection, Sephora offers beauty products including
+          cosmetics, skincare, body, fragrance, nail color, beauty tools, body
+          lotions and haircare
+        </span>
+      </h1>
     </div>
   );
 }
