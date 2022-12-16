9b4f80f34666aba1c69b4cba1a01258f0db4802d
Therobo77 <anshusinha8873@gmail.com>
2022-12-16 08:43:48 +0530

    SEE

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ function App() {
       <h1>Project Code :zealous-fruit-8632</h1>
       <h1>Stay Connected</h1>
 
-      <h1>
-        # zealous-fruit-8632 `Project Code` ## About Project
-        <br/>
-        <span>
-          Sephora is a French multinational retailer of personal care and beauty
-          products. Featuring nearly 340 brands, along with its own private
-          label, Sephora Collection, Sephora offers beauty products including
-          cosmetics, skincare, body, fragrance, nail color, beauty tools, body
-          lotions and haircare
-        </span>
-      </h1>
+      
       <login/>
     </div>
   );
