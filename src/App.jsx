df6c37679053bc12a7a3b1740282cc73a507b62d
Raj210Kumari <rajnandani02101997@gmail.com>
2022-12-17 16:34:25 +0530

    LoginUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
-// import { ProductDetails } from "./Componants/Product-Detail-Page/ProductDetails";
-// import { HairProduct } from "./Componants/Product-Page/HairProduct";
-// import { MackupProduct } from "./Componants/Product-Page/MackUpProduct";
-import { Home } from "./Componants/Home/Home";
-// import { Login } from "./Componants/login/login";
-
-// import { CheckOutPage } from "./Componants/checkoutpage/CheckoutPage";
-// import { Payment } from "./Componants/shippingPaymentDet/PaymentPage";
-// import { OTP } from "./Componants/login/otp";
-// import { Signup } from "./Componants/login/Signup";
+import MainRoute from "./AllRoutes/MainRoute";
 
 function App() {
   return (
     <div className="App">
-
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/login" element={<Login />}></Route> */}
-        {/* <Route path="/otp" element={<OTP />}></Route> */}
-        {/* <Route path="/signup" element={<Signup />}></Route> */}
-
-        {/* <Route path="/hairproduct" element={<HairProduct />}></Route> */}
-        {/* <Route path="/mackupproduct" element={<MackupProduct />}></Route> */}
-        {/* <Route path="/productdetail" element={<ProductDetails />}></Route> */}
-        {/* <Route path="/checkoutpage" element={<CheckOutPage />}></Route> */}
-        <Route path="/home" element={<Home />}></Route>
-        {/* <Route path="/payment" element={<Payment />}></Route> */}
-      </Routes>
+      <h1>Work in progress !</h1>
+      <h1>Project Code :zealous-fruit-8632</h1>
+      <h1>Stay Connected</h1>
+      <MainRoute/>
     </div>
   );
 }
