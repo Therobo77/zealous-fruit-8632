77bd56197b437a5c162be0654fa4f6e36bd0726b
NITESH SINDHU <niteshsindhu1435@gmail.com>
2022-12-17 22:07:13 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+// import { Login } from "./Componants/Login/Login";
 import Admin from "./Nitesh/Nitesh/Admin";
 
 function App() {
   return (
     <div className="App">
-      <Admin />
+          <Admin />
+      {/* <Login/> */}
     </div>
   );
 }
