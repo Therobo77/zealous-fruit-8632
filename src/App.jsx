bbd8c74a480e2725e7f47ddecbd9399363236862
Therobo77 <anshusinha8873@gmail.com>
2022-12-17 14:29:03 +0530

    testing Pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import { ProductDetails } from "./Componants/Product-Detail-Page/ProductDetails";
-import { HairProduct } from "./Componants/Product-Page/HairProduct";
-import { MackupProduct } from "./Componants/Product-Page/MackUpProduct";
+// import { ProductDetails } from "./Componants/Product-Detail-Page/ProductDetails";
+// import { HairProduct } from "./Componants/Product-Page/HairProduct";
+// import { MackupProduct } from "./Componants/Product-Page/MackUpProduct";
 import { Home } from "./Componants/Home/Home";
-import { Login } from "./Componants/login/login";
+// import { Login } from "./Componants/login/login";
 
-import { CheckOutPage } from "./Componants/checkoutpage/CheckoutPage";
-import { Payment } from "./Componants/shippingPaymentDet/PaymentPage";
-import { OTP } from "./Componants/login/otp";
-import { Signup } from "./Componants/login/Signup";
+// import { CheckOutPage } from "./Componants/checkoutpage/CheckoutPage";
+// import { Payment } from "./Componants/shippingPaymentDet/PaymentPage";
+// import { OTP } from "./Componants/login/otp";
+// import { Signup } from "./Componants/login/Signup";
 
 function App() {
   return (
