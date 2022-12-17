2f604175bbb796d7b5b4aaf19dec30846443d488
NITESH SINDHU <niteshsindhu1435@gmail.com>
2022-12-17 22:52:53 +0530

    final

--- src/AllRoutes/MainRoute.jsx
+++ src/AllRoutes/MainRoute.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Login } from '../Components/Login/Login'
-import {AuthContextOTP} from "../Components/Login/AuthContextOTP"
-import {Register} from '../Components/Register/Register'
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Login } from '../Components/Login/Login'
+// import {AuthContextOTP} from "../Components/Login/AuthContextOTP"
+// import {Register} from '../Components/Register/Register'
 import { Routes, Route } from "react-router-dom"
-import { Home } from "./Componants/Home/Home";
+import { Home } from '../Componants/Home/Home'
+import Login from '../Components/Login'
+import Admin from '../Nitesh/Nitesh/Admin'
+// import { Home } from "./Componants/Home/Home";
 
 const MainRoute = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/authContextOTP" element={<AuthContextOTP />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/home" element={<Home />}></Route>
+        {/* <Route path="/authContextOTP" element={<AuthContextOTP />}></Route> */}
+        {/* <Route path="/register" element={<Register />}></Route> */}
+        {/* <Route path="/home" element={<Home />}></Route> */}
       </Routes>
     </div>
   )
